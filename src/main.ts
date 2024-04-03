81f77a383482220cc4f5cb6c73962331105d461e
Cristian Robles <cristian210206.jcrd@gmail.com>
2024-04-03 11:28:06 +0200

    Calculos cena de cumpleaños

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import "./style.css";
+
+console.log(
+  "Somos 6 personas, el precio de la cena con el descuento del ticket seria " +
+    (120 - 18) +
+    "€, cada persona debera pagar " +
+    (120 - 18) / 6 +
+    "€ para que los costos se dividan de manera equitativa"
+);
